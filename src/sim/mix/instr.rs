6ad83c6b35d529f00a4103f10cc5a7740f38f398
Rong Mantle Bao <baorong2005@126.com>
2022-05-19 17:40:18 +0800

    Switch to genuine unit tests

--- src/sim/mix/instr.rs
+++ src/sim/mix/instr.rs
@@ -516,3 +516,29 @@ impl ToRangeInclusive<usize> for u8 {
         ((self / 8) as usize)..=((self % 8) as usize)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::sim::mix::instr::*;
+    use crate::sim::mix::mem::*;
+
+    #[test]
+    fn test_instruction_from() {
+        let mut word = Word::<6, false>::new();
+        word.set(0..=5, &[0, 0x07, 0xD0, 0x02, 0x03, 0x08]).unwrap();
+
+        let instr = Instruction::try_from(word).unwrap();
+        assert_eq!(instr.opcode, Opcode::LdA);
+        assert_eq!(instr.field, 3);
+        assert_eq!(instr.index, 2);
+        assert_eq!(instr.addr, 2000);
+    }
+
+    #[test]
+    fn test_instruction_into() {
+        let instr = Instruction::new(2000, 0x03, 0x02, Opcode::LdA);
+
+        let word: Word<6, false> = instr.try_into().unwrap();
+        assert_eq!(word[0..=5], [0, 0x07, 0xD0, 0x02, 0x03, 0x08]);
+    }
+}
--- src/sim/mix/mem.rs
+++ src/sim/mix/mem.rs
@@ -230,3 +230,57 @@ impl std::ops::IndexMut<u16> for Mem {
         &mut self.data[index as usize]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::sim::mix::mem::*;
+
+    #[test]
+    fn test_word_mutation() {
+        let mut reg = Word::<6, false>::new();
+
+        assert_eq!(reg[0..6], [0; 6]);
+        reg.set(0..=5, &[1, 2, 3, 4, 5, 6]).unwrap();
+        assert_eq!(reg[0..6], [1, 2, 3, 4, 5, 6]);
+        reg.set(0..=2, &[1, 8, 9]).unwrap();
+        assert_eq!(reg[0..6], [1, 8, 9, 4, 5, 6]);
+        reg.set(2..=5, &[1, 2, 3, 4]).unwrap();
+        assert_eq!(reg[0..6], [1, 8, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_word_pos_sign_mutation() {
+        let mut reg = Word::<3, true>::new();
+
+        assert_eq!(reg[0..3], [1, 0, 0]);
+        reg.set(0..=2, &[1, 2, 3]).unwrap();
+        assert_eq!(reg[0..3], [1, 2, 3]);
+        reg.set(0..=2, &[0, 0, 0]).unwrap();
+        assert_eq!(reg[0..3], [1, 0, 0]);
+    }
+
+    #[test]
+    fn test_word_interchange() {
+        let mut reg1 = Word::<6, false>::new();
+        let mut reg2 = Word::<6, false>::new();
+        reg1.set(0..=5, &[1, 2, 3, 4, 5, 6]).unwrap();
+        reg2.set(0..=5, &[1, 8, 9, 10, 11, 12]).unwrap();
+
+        reg1.set(3..=5, &reg2[3..6]).unwrap();
+        assert_eq!(reg1[0..6], [1, 2, 3, 10, 11, 12]);
+        assert_eq!(reg2[0..6], [1, 8, 9, 10, 11, 12]);
+        reg2.set(0..=2, &reg1[0..3]).unwrap();
+        assert_eq!(reg1[0..6], [1, 2, 3, 10, 11, 12]);
+        assert_eq!(reg2[0..6], [1, 2, 3, 10, 11, 12]);
+    }
+
+    #[test]
+    fn test_word_error_handling() {
+        let mut reg = Word::<6, false>::new();
+
+        assert_eq!(reg.set(8..=0, &[]), Err(()));
+        assert_eq!(reg.set(0..=100, &[]), Err(()));
+        assert_eq!(reg.set(0..=2, &[]), Err(()));
+        assert_eq!(reg.set(0..=2, &[1, 2, 3, 4, 5, 6, 7]), Err(()));
+    }
+}
